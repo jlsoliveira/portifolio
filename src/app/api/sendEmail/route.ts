76f4683f7a31b9a5bc00f2126d1c3f7c220aacfa
Hanna Liz <hannaliz@MacBook-Pro-de-Hanna.local>
2024-08-24 18:48:32 -0300

    fix: sendEmail

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -1,9 +1,8 @@
-// app/api/sendEmail/route.ts
-import { NextResponse } from 'next/server';
-import nodemailer from 'nodemailer';
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: 'hotmail',
+  service: "hotmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -18,19 +17,25 @@ export async function POST(request: Request) {
     const { name, email, message } = await request.json();
 
     if (!name || !email || !message) {
-      return NextResponse.json({ message: 'All fields are required.' }, { status: 400 });
+      return NextResponse.json(
+        { message: "All fields are required." },
+        { status: 400 }
+      );
     }
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
-      to: 'hanna.liz.oliveira@hotmail.com',
+      to: "hanna.liz.oliveira@hotmail.com",
       subject: `Contact Form Submission: ${message}`,
       text: `Name: ${name}\nE-mail: ${email}\nMessage: ${message}`,
     });
 
-    return NextResponse.json({ message: 'Email sent successfully!' });
+    return NextResponse.json({ message: "Email sent successfully!" });
   } catch (error) {
-    console.error('Error sending email:', error);
-    return NextResponse.json({ message: 'Failed to send email.' }, { status: 500 });
+    console.error("Error sending email:", error);
+    return NextResponse.json(
+      { message: "Failed to send email." },
+      { status: 500 }
+    );
   }
 }
